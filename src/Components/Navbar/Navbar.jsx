4e89160fa170270e7c3d3ae1ed9085ac662d6761
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-24 02:49:29 +0600

    added search btn to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -38,6 +38,24 @@ const Navbar = () => {
               <li>
                 <a href="login">Login</a>
               </li>
+              <li>
+                <button className="btn btn-ghost btn-circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+              </li>
             </ul>
           </div>
           <a
@@ -60,6 +78,24 @@ const Navbar = () => {
 
             <li>
               <a href="/dashboard">Dashboard</a>
+            </li>
+            <li>
+              <button className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
             </li>
           </ul>
 
