4651334d50927acfc540b31c19ab76770b825e47
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-01-24 03:58:01 +0600

    added chakra ui card in houses

--- src/Components/Houses/Houses.jsx
+++ src/Components/Houses/Houses.jsx
@@ -1,49 +1,67 @@
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const Houses = () => {
   const [allHouse, setAllHouse] = useState([]);
 
   useEffect(() => {
-    fetch('public/assets/data.json')
+    fetch('public/data.json')
       .then((res) => res.json())
       .then((data) => setAllHouse(data));
   }, []);
 
   return (
-    <div id="courses" className="pb-12">
+    <div id="houses" className="pb-12">
       <h1 className="text-center py-12 lg:py-20 text-2xl lg:text-5xl font-bold">
         Featured Courses
       </h1>
 
       <div className="container mx-auto">
-        {/* Search Input */}
-        <input
-          type="text"
-          placeholder="Search by name or instructor"
-          className="form-input ml-5 rounded-lg border-blue-700 border-opacity-50 border-2 my-7 p-3 px-9"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-
         <div className="home-container gap-5">
           <div className="card-container place-items-center grid grid-cols-1 lg:grid-cols-3 gap-4">
             {allHouse.map((house) => (
-              <div
-                key={house.id}
-                className="card w-96 border-2 bg-gray-50 rounded-lg p-2 mb-4 "
-              >
-                <img
-                  className="rounded-md p-3 w-full"
-                  src={house.picture}
-                  alt=""
-                />
-
-                <Link to={`/details/${course._id}`}>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-36 py-2 mx-3 my-2 rounded">
-                    Details
-                  </button>
-                </Link>
-              </div>
+              <Card key={house.id} maxW="sm">
+                <CardBody>
+                  <Image
+                    src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                    alt="Green double couch with wooden legs"
+                    borderRadius="lg"
+                  />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="md">Living room Sofa</Heading>
+                    <Text>
+                      This sofa is perfect for modern tropical spaces, baroque
+                      inspired spaces, earthy toned spaces and for people who
+                      love a chic design with a sprinkle of vintage design.
+                    </Text>
+                    <Text color="blue.600" fontSize="2xl">
+                      $450
+                    </Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button variant="solid" colorScheme="blue">
+                      Buy now
+                    </Button>
+                    <Button variant="ghost" colorScheme="blue">
+                      Add to cart
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
             ))}
           </div>
         </div>
